a34e70e8c9399aad3df7f896187fa779f49e4bc4
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-10-28 18:42:57 -0500

    updating for data to come from provided mock info

--- server/src/index.js
+++ server/src/index.js
@@ -3,27 +3,31 @@ const typeDefs = require("./schema");
 
 const mocks = {
   Query: () => ({
-    tracksForHome: () => [...new Array(7)],
+    tracksForHome: () => [...new Array(6)]
   }),
   Track: () => ({
-    id: () => "track_01",
-    title: () => "Astro Kitty, Space Explorer",
+    id: () => 'track_01',
+    title: () => 'Astro Kitty, Space Explorer',
     author: () => {
       return {
-        name: "Grumpy Cat",
+        name: 'Grumpy Cat',
         photo:
-          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg'
       };
     },
     thumbnail: () =>
-      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
     length: () => 1210,
-    modulesCount: () => 6,
-  }),
+    modulesCount: () => 6
+  })
 };
 
-const server = new ApolloServer({ typeDefs, mocks: true });
+// mocks: true is only if you want random data but if you make a mock variable remove true like so:
+const server = new ApolloServer({ 
+  typeDefs, mocks 
+});
 
+// make sure you are in server then npm start
 server.listen().then(() => {
   console.log(`
       🚀  Server is running!
