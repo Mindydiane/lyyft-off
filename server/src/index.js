010511204ca5924860a6278b4fe766d3949244dd
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-10-28 22:01:05 -0500

    need to have both client and server running at the same time for it to work,up and running update

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,9 @@
-const { ApolloServer } = require("apollo-server");
-const typeDefs = require("./schema");
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
 
 const mocks = {
   Query: () => ({
-    tracksForHome: () => [...new Array(6)]
+    tracksForHome: () => [...new Array(9)],
   }),
   Track: () => ({
     id: () => 'track_01',
@@ -12,26 +12,25 @@ const mocks = {
       return {
         name: 'Grumpy Cat',
         photo:
-          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg'
+          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
       };
     },
     thumbnail: () =>
       'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
     length: () => 1210,
-    modulesCount: () => 6
-  })
+    modulesCount: () => 6,
+  }),
 };
 
-// mocks: true is only if you want random data but if you make a mock variable remove true like so:
-const server = new ApolloServer({ 
-  typeDefs, mocks 
+const server = new ApolloServer({
+  typeDefs,
+  mocks,
 });
 
-// make sure you are in server then npm start
 server.listen().then(() => {
   console.log(`
-      🚀  Server is running!
-      🔉  Listening on port 4000
-      📭  Query at https://studio.apollographql.com/dev
-    `);
+    🚀  Server is running!
+    🔉  Listening on port 4000
+    📭  Query at https://studio.apollographql.com/dev
+`);
 });
